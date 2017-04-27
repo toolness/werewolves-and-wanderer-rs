8fd4d10b45eb5675b578fcf5a24df451ac273e31
Atul Varma <varmaa@gmail.com>
2017-04-27 19:16:04 -0400

    Convert GameMap functions into methods.

--- src/game_map.rs
+++ src/game_map.rs
@@ -58,6 +58,82 @@ pub enum RoomContents {
 
 pub type GameMap<'a> = map::Map<'a, RoomId, RoomContents>;
 
+impl<'a> GameMap<'a> {
+  fn allot_terror(&mut self) {
+    for _ in 0..NUM_ROOMS_WITH_TERROR {
+      loop {
+        let room_id = random_room_id();
+        if room_id != Entrance && room_id != Exit {
+          let room = self.room(room_id);
+          if room.contents.is_none() {
+            let monster_id = random_monster_id();
+            if DEBUG {
+              println!("DEBUG: Placing {:?} in {:?}.", monster_id, room_id);
+            }
+            room.contents = Some(Terror(monster_id));
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  fn allot_treasure(&mut self) {
+    for _ in 0..NUM_ROOMS_WITH_TREASURE {
+      loop {
+        let room_id = random_room_id();
+        if room_id != Entrance && room_id != Exit {
+          let room = self.room(room_id);
+          if room.contents.is_none() {
+            let amount = random_treasure_amount();
+            if DEBUG {
+              println!("DEBUG: Placing ${} in {:?}.", amount, room_id);
+            }
+            room.contents = Some(Treasure(amount));
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  fn ensure_treasure(&mut self) {
+    for &room_id in [Treasury, PrivateMeeting].iter() {
+      let amount = random_treasure_amount();
+      if DEBUG {
+        println!("DEBUG: Placing ${} in {:?}.", amount, room_id);
+      }
+      self.room(room_id).contents = Some(Treasure(amount));
+    }
+  }
+
+  fn describe_and_connect(&mut self) {
+    self.room(Entrance).describe(
+      "Entrance",
+      "You are at the entrance to a forbidding-looking \
+       stone castle. You are facing east."
+    );
+
+    self.room(Hallway).describe(
+      "Hallway",
+      "You are in the hallway. \
+       There is a door to the south. \
+       Through the windows to the north you can see a secret herb garden."
+    );
+
+    self.room(AudienceChamber).describe(
+      "Audience Chamber",
+      "This is the audience chamber. \
+       There is a window to the west. By looking to the right \
+       through it you can see the entrance to the castle. \
+       Doors leave this room to the north, east, and south."
+    );
+
+    self.connect(Entrance, East, Hallway);
+    self.connect(Hallway, South, AudienceChamber);
+  }
+}
+
 fn random_room_id() -> RoomId {
   loop {
     let r = random_i32(0, NUM_ROOMS as i32);
@@ -82,80 +158,6 @@ fn random_treasure_amount() -> u8 {
   random_i32(MIN_TREASURE_AMOUNT as i32, MAX_TREASURE_AMOUNT as i32) as u8
 }
 
-fn allot_terror(map: &mut GameMap) {
-  for _ in 0..NUM_ROOMS_WITH_TERROR {
-    loop {
-      let room_id = random_room_id();
-      if room_id != Entrance && room_id != Exit {
-        let room = map.room(room_id);
-        if room.contents.is_none() {
-          let monster_id = random_monster_id();
-          if DEBUG {
-            println!("DEBUG: Placing {:?} in {:?}.", monster_id, room_id);
-          }
-          room.contents = Some(Terror(monster_id));
-          break;
-        }
-      }
-    }
-  }
-}
-
-fn allot_treasure(map: &mut GameMap) {
-  for _ in 0..NUM_ROOMS_WITH_TREASURE {
-    loop {
-      let room_id = random_room_id();
-      if room_id != Entrance && room_id != Exit {
-        let room = map.room(room_id);
-        if room.contents.is_none() {
-          let amount = random_treasure_amount();
-          if DEBUG {
-            println!("DEBUG: Placing ${} in {:?}.", amount, room_id);
-          }
-          room.contents = Some(Treasure(amount));
-          break;
-        }
-      }
-    }
-  }
-}
-
-fn ensure_treasure(map: &mut GameMap) {
-  for &room_id in [Treasury, PrivateMeeting].iter() {
-    let amount = random_treasure_amount();
-    if DEBUG {
-      println!("DEBUG: Placing ${} in {:?}.", amount, room_id);
-    }
-    map.room(room_id).contents = Some(Treasure(amount));
-  }
-}
-
-fn build_world(map: &mut GameMap) {
-  map.room(Entrance).describe(
-    "Entrance",
-    "You are at the entrance to a forbidding-looking \
-     stone castle. You are facing east."
-  );
-
-  map.room(Hallway).describe(
-    "Hallway",
-    "You are in the hallway. \
-     There is a door to the south. \
-     Through the windows to the north you can see a secret herb garden."
-  );
-
-  map.room(AudienceChamber).describe(
-    "Audience Chamber",
-    "This is the audience chamber. \
-     There is a window to the west. By looking to the right \
-     through it you can see the entrance to the castle. \
-     Doors leave this room to the north, east, and south."
-  );
-
-  map.connect(Entrance, East, Hallway);
-  map.connect(Hallway, South, AudienceChamber);
-}
-
 // Ideally we'd actually just get rid of MapRoomId and add a constraint
 // to Map<T> requiring that T be type-castable as usize, but I don't
 // know how to do that, so...
@@ -164,8 +166,8 @@ impl map::MapRoomId for RoomId {
 }
 
 pub fn init(mut map: &mut GameMap) {
-  build_world(&mut map);
-  allot_treasure(&mut map);
-  allot_terror(&mut map);
-  ensure_treasure(&mut map);
+  map.describe_and_connect();
+  map.allot_treasure();
+  map.allot_terror();
+  map.ensure_treasure();
 }
