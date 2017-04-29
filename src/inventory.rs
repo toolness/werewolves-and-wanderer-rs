1fb668bace0982b3324bb9f36b959ccae1fcfed1
Atul Varma <varmaa@gmail.com>
2017-04-29 19:57:25 -0400

    Add axe, sword, amulet to shop.

--- src/inventory.rs
+++ src/inventory.rs
@@ -7,12 +7,18 @@ use self::Item::*;
 
 pub enum Item {
   Torch,
+  Axe,
+  Sword,
+  Amulet,
 }
 
 impl Item {
   pub fn price(&self) -> i32 {
     match *self {
-      Torch => 15
+      Torch => 15,
+      Axe => 10,
+      Sword => 20,
+      Amulet => 30,
     }
   }
 }
@@ -21,6 +27,9 @@ impl fmt::Display for Item {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     write!(f, "{}", match *self {
       Torch => "a flaming torch",
+      Axe => "an axe",
+      Sword => "a sword",
+      Amulet => "the magic amulet",
     })
   }
 }
@@ -38,6 +47,9 @@ impl CommandProcessor<InventoryCommand> for InventoryCommand {
 
     HelpInfo::list(vec![
       ('1', buy(Torch)),
+      ('2', buy(Axe)),
+      ('3', buy(Sword)),
+      ('5', buy(Amulet)),
       ('0', String::from("continue adventure")),
     ])
   }
@@ -45,6 +57,9 @@ impl CommandProcessor<InventoryCommand> for InventoryCommand {
   fn from_char(c: char) -> Option<InventoryCommand> {
     match c {
       '1' => Some(InventoryCommand::Buy(Torch)),
+      '2' => Some(InventoryCommand::Buy(Axe)),
+      '3' => Some(InventoryCommand::Buy(Sword)),
+      '5' => Some(InventoryCommand::Buy(Amulet)),
       _ => Some(InventoryCommand::Quit),
     }
   }
@@ -73,6 +88,9 @@ impl<'a> GameState<'a> {
             self.print_wealth();
             match item {
               Torch => self.light = true,
+              Axe => self.axe = true,
+              Sword => self.sword = true,
+              Amulet => self.amulet = true,
             }
           }
           println!("");
