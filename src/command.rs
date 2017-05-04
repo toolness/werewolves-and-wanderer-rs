a2f646751c2050a30995009a0257fec3c27ba067
Atul Varma <varmaa@gmail.com>
2017-05-03 21:22:42 -0400

    Factor out CommandProcessor::get_from_input().

--- src/command.rs
+++ src/command.rs
@@ -29,32 +29,32 @@ pub trait CommandProcessor<T> {
     }
   }
 
+  fn get_from_input(input: String) -> Option<T> {
+    platform::hide_prompt();
+    match input.chars().next() {
+      Some(k) => {
+        let k = k.to_ascii_lowercase();
+        if k == 'h' || k == '?' {
+          println!("Here's what I understand right now:\n");
+          Self::show_help();
+          println!("");
+          return None;
+        } else if let Some(cmd) = Self::from_char(k) {
+          return Some(cmd);
+        }
+      },
+      None => {}
+    }
+    println!("I have no idea what you're talking about.");
+    return None;
+  }
+
   fn get() -> Option<T> {
     platform::show_prompt(Self::prompt());
 
     match platform::read_input() {
-      Some(input) => {
-        platform::hide_prompt();
-        match input.chars().next() {
-          Some(k) => {
-            let k = k.to_ascii_lowercase();
-            if k == 'h' || k == '?' {
-              println!("Here's what I understand right now:\n");
-              Self::show_help();
-              println!("");
-              return None;
-            } else if let Some(cmd) = Self::from_char(k) {
-              return Some(cmd);
-            }
-          },
-          None => {}
-        }
-        println!("I have no idea what you're talking about.");
-        return None;
-      },
-      None => {
-        return None;
-      }
+      Some(input) => Self::get_from_input(input),
+      None => None
     }
   }
 }
